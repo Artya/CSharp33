4d0e63bbc012b730008f512e89144c153ec81571
kievzenit <t.u.t.u.h.a.agent@gmail.com>
2021-12-03 15:43:04 +0200

    Added files

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int previous;
+            int current;
+            string operation;
+
+            while(true)
+            {
+                StartCalculation:
+                while(true)
+                {
+                    Console.Write("Введите число: ");
+                    var priv = Console.ReadLine();
+                    try
+                    {
+                        previous = int.Parse(priv);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Это не число");
+                    }
+                }
+
+                while(true)
+                {
+                    Console.Write("Введите операцию: ");
+                    operation = Console.ReadLine();
+                    if (operation == "!")
+                    {
+                        Console.WriteLine($"Факториал числа {previous} равен {Factorial(previous)}");
+                        goto StartCalculation;
+                    }
+                    if (operation == "+" || operation == "-" || operation == "*" || operation == "/")
+                        break;
+                    else
+                        Console.WriteLine("Неверная операция, доступны только: + - * / !");
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите число: ");
+                    var curr = Console.ReadLine();
+                    try
+                    {
+                        current = int.Parse(curr);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Это не число");
+                    }
+                }
+
+                switch(operation)
+                {
+                    case "+":
+                        Console.WriteLine($"{previous} + {current} = {previous + current}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{previous} - {current} = {previous - current}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{previous} * {current} = {previous * current}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{previous} / {current} = {previous / current}");
+                        break;
+                }
+            }
+        }
+
+        static int Factorial(int n)
+        {
+            if (n <= 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
