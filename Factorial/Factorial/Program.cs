cbee6b17acb87ac0131a9c879fe3aad4fdb8874c
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2021-12-04 16:14:11 +0200

    no message

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number please:");
+
+            var input = Console.ReadLine();
+            var number = int.Parse(input);
+
+            int result = 1;
+
+            if (number > 1)
+            {
+                for (var index = number; index > 1; index--)
+                {
+                    result *= index;
+                }
+            }
+
+            Console.WriteLine(number.ToString() + "! = " + result.ToString());
+        }
+    }
+}
