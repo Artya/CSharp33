23519eee0cf368202061cbd7d01d9d89dec915a6
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2021-12-05 14:56:21 +0200

    It empty

---
+++ Lab_1_5/Lab_1_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
