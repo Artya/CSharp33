f3fda7a138eb73050f47eb07854024dee7234356
kievzenit <t.u.t.u.h.a.agent@gmail.com>
2021-12-09 17:12:04 +0200

    Added files

---
+++ Lab 1.2.1/Lab 1.2.1/LibraryUser.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_1._2._1
+{
+    public interface ILibraryUser
+    {
+        void AddBook(string bookName);
+        void RemoveBook(string bookName);
+        string GetBookInfo(int index);
+        int GetBooksCount();
+    }
+
+    public class LibraryUser : ILibraryUser
+    {
+        private static int nextId;
+        public static int GetNextId()
+        {
+            return nextId++;
+        }
+
+        public string Phone { get; set; }
+        public int BookCount { get => this.bookList.Count; }
+
+        public readonly string FirstName;
+        public readonly string LastName;
+        public readonly int Id;
+        public readonly int BookLimit;
+        public readonly bool IsAnonymous;
+
+        private List<string> bookList;
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= this.BookCount || index >= this.BookLimit || index < 0)
+                    throw new IndexOutOfRangeException();
+                return this.bookList[index];
+            }
+            set
+            {
+                if (index >= this.BookCount || index >= this.BookLimit || index < 0)
+                    throw new IndexOutOfRangeException();
+                this.bookList[index] = value;
+            }
+        }
+
+        public LibraryUser()
+        {
+            this.FirstName = "No name";
+            this.LastName = "No last name";
+            this.Phone = "No phone";
+            this.Anonymous = true;
+            this.BookLimit = 10;
+            this.Id = LibraryUser.GetNextId();
+            this.bookList = new List<string>();
+        }
+
+        public LibraryUser(string firstName, string lastName, string phone, int bookLimit)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+            this.Anonymous = false;
+            this.BookLimit = bookLimit;
+            this.Id = LibraryUser.GetNextId();
+            this.bookList = new List<string>();
+        }
+
+        public void AddBook(string bookName)
+        {
+            this.bookList.Add(bookName);
+        }
+
+        public void RemoveBook(string bookName)
+        {
+            this.bookList.Remove(bookName);
+        }
+
+        public string GetBookInfo(int index)
+        {
+            return this[index];
+        }
+
+        public int GetBooksCount()
+        {
+            return this.BookCount;
+        }
+
+        public void PrintAllBooksName()
+        {
+            for (int bookIndex = 0; bookIndex < this.BookCount; bookIndex++)
+                Console.WriteLine(@$"    {this[bookIndex]}");
+        }
+    }
+}
---
+++ Lab 1.2.1/Lab 1.2.1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Lab_1._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user1 = new LibraryUser();
+            const int bookLimit = 300;
+            var user2 = new LibraryUser("Maria", "Ivanenko", "+380447777777", bookLimit);
+
+            Console.WriteLine("user1 " + user1.FirstName + ", " + user1.LastName + ", anonymous: " + user1.IsAnonymous);
+            Console.WriteLine("user2 " + user2.FirstName + ", " + user2.LastName + ", anonymous: " + user2.IsAnonymous);
+
+            Console.WriteLine("user1: add Harry Potter");
+            user1.AddBook("Harry Potter");
+            Console.WriteLine("user2: add Sherlock Holmes");
+            user2.AddBook("Sherlock Holmes");
+
+            Console.WriteLine("user1.BooksCount = " + user1.GetBooksCount());
+            Console.WriteLine("user2.BooksCount = " + user2.GetBooksCount());
+
+            Console.WriteLine("user2: add Kobzar");
+            user2.AddBook("Kobzar");
+            Console.WriteLine("user2.BooksCount = " + user2.GetBooksCount());
+            Console.WriteLine("user2: add Dorian Gray");
+            user2.AddBook("Dorian Gray");
+            Console.WriteLine("user2.BooksCount = " + user2.GetBooksCount());
+
+            Console.WriteLine("user1 books:");
+            user1.PrintAllBooksName();
+            Console.WriteLine("user2 books:");
+            user2.PrintAllBooksName();
+
+            Console.WriteLine($"Book info for user1 with index 0: {user1.GetBookInfo(0)}");
+            Console.WriteLine($"Book info for user2 with index 1: {user2.GetBookInfo(1)}");
+
+            Console.WriteLine("user1: remove Harry Potter");
+            user1.RemoveBook("Harry Potter");
+            Console.WriteLine("user1.BooksCount = " + user1.GetBooksCount());
+            Console.WriteLine("user2: remove Sherlock Holmes");
+            user2.RemoveBook("Sherlock Holmes");
+            Console.WriteLine("user2.BooksCount = " + user2.GetBooksCount());
+
+            Console.WriteLine("user2 books:");
+            user2.PrintAllBooksName();
+
+            Console.ReadLine();
+        }
+    }
+}
