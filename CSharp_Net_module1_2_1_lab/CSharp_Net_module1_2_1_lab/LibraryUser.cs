7b835fb83c7f3dadf7d0440c8f7581eb41c5e6e6
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2021-12-11 14:20:12 +0200

    Library simulator

---
+++ CSharp_Net_module1_2_1_lab/CSharp_Net_module1_2_1_lab/LibraryUser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace CSharp_Net_module1_2_1_lab
+{
+
+    // 1) declare interface ILibraryUser
+    public interface ILibraryUser
+    {
+        // declare method's signature for methods of class LibraryUser
+        bool AddBook(string bookName);
+        void RemoveBook(string bookName);
+        string BookInfo(int bookIndex);
+        int BooksCount();
+    }
+
+    // 2) declare class LibraryUser, it implements ILibraryUser
+    public class LibraryUser : ILibraryUser
+    {
+        private static int objectCounter = 0;
+
+        private readonly string firstName;
+        private readonly string lastName;
+        private readonly int id;
+        private readonly int bookLimit;
+        // 4) declare field (bookList) as a string array
+        private string[] bookList;
+
+
+        // 3) declare properties: FirstName (read only), LastName (read only), 
+        // Id (read only), Phone (get and set), BookLimit (read only)
+        public string FirstName { get => firstName; }
+        public  string LastName { get => lastName; }
+
+        public int ID { get => id;  }
+
+        public string Phone { get; set; }
+
+        public int BookLimit { get => bookLimit;  }
+       
+        // 5) declare indexer BookList for array bookList
+         public string this[int index]
+        {
+            get
+            {
+                if (ChekIndexInRange(index))
+                    return bookList[index];
+
+                throw new IndexOutOfRangeException("Index incorrect");
+            }
+        }
+
+        // 6) declare constructors: default and parameter
+        public LibraryUser()
+        {
+            this.id = ++objectCounter;
+            this.firstName = "<No name>";
+            this.lastName = "<No last name>";
+            this.bookLimit = 1;
+            this.Phone = "<no phone>";
+
+            this.bookList = new string[bookLimit];
+        }
+
+        public LibraryUser(string firstName, string lastName, string phone, int bookLimit)
+        {
+            this.id = ++objectCounter;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.bookLimit = bookLimit;
+            this.Phone = phone;
+
+            this.bookList = new string[bookLimit];
+        }
+
+        // 7) declare methods: 
+        public bool AddBook(string bookName) // – add new book to array bookList
+        {
+            var booksCount = BooksCount();
+
+            if (booksCount >= bookLimit)
+            {
+                Console.WriteLine("Sorry, but you already have maximum count of books... Maybe you need to remove some books?");
+                return false;
+            }
+
+            bookList[booksCount] = bookName;
+            return true;
+
+        }
+        public void RemoveBook(string bookName)// – remove book from array bookList
+        {
+            var bookFound = false;
+
+            for (var i = 0; i < bookLimit; i++)
+            {
+                if (!bookFound && bookName == bookList[i])
+                {
+                    bookFound = true;
+                    bookList[i] = null;
+                }
+
+                if (bookFound && (i + 1) < bookLimit)
+                {
+                    bookList[i] = bookList[i + 1];
+                    bookList[i + 1] = null;
+                }
+            }
+        }
+        public string BookInfo(int bookIndex)// – returns book info by index
+        {
+            if (ChekIndexInRange(bookIndex))
+               return bookList[bookIndex];
+
+            throw new IndexOutOfRangeException ("Incorrect index");
+        }
+        public int BooksCount()// – returns current count of books
+        {
+            for (var i = 0; i < bookLimit; i++)
+            {
+                if (bookList[i] == null)
+                    return i;
+            }
+
+            return bookLimit;
+        }
+
+        private bool ChekIndexInRange(int index)
+        {
+            return index >= 0 && index < bookList.Length;
+        }
+    }
+  
+}
---
+++ CSharp_Net_module1_2_1_lab/CSharp_Net_module1_2_1_lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp_Net_module1_2_1_lab
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 8) declare 2 objects. Use default and paremeter constructors
+            var user1 = new LibraryUser();
+            var user2 = new LibraryUser("Maria", "Ivanenko", "+380447777777", 3); // in template 3-rd argument was 2, but in my logic it book limit, so I chsnged it to 3
+            Console.WriteLine("User1 ID " + user1.ID + " " + user1.FirstName + " " + user1.LastName);
+            Console.WriteLine ("User2 ID " + user2.ID + " " + user2.FirstName + " " + user2.LastName);
+                
+            // 9) do operations with books for all users: run all methods for both objects
+            Console.WriteLine("User 1: add Harry Potter");
+            user1.AddBook("Harry Potter");
+            Console.WriteLine("User 2: add Sherlock Holmes");
+            user2.AddBook("Sherlock Holmes");
+            Console.WriteLine("user1.BooksCount = " + user1.BooksCount() + "; user2.BooksCount " + user2.BooksCount());
+            Console.WriteLine("user2 :");
+            Console.WriteLine("Add Kobzar");
+            user2.AddBook("Kobzar");
+            Console.WriteLine("user2.BooksCount " + user2.BooksCount());
+            Console.WriteLine("Add Dorian Gray");
+            user2.AddBook("Dorian Gray");
+            Console.WriteLine("user2.BooksCount " + user2.BooksCount());
+
+            Console.WriteLine("user2 books: " + user2[0] + "; " + user2[1] + "; " + user2[2]);
+            Console.WriteLine("user2 books: " + user2.BookInfo(0) + "; " + user2.BookInfo(1) + "; " + user2.BookInfo(2));
+            Console.WriteLine("Remove Sherlock Holmes");
+            user2.RemoveBook("Sherlock Holmes");
+            Console.WriteLine("user2.BooksCount "+user2.BooksCount());
+            Console.WriteLine("user2 books: " + user2[0] + "; " + user2[1]);
+            Console.WriteLine("user2 books: " + user2.BookInfo(0) + "; " + user2.BookInfo(1));
+
+        }
+    }
+}
