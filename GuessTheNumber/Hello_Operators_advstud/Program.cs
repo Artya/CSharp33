8a62a8791d795b5e8953379481d1ea26b2b495c7
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2021-12-26 15:50:14 +0200

    Code style fixed

--- GuessTheNumber/Hello_Operators_advstud/Program.cs
+++ GuessTheNumber/Hello_Operators_advstud/Program.cs
@@ -16,18 +16,18 @@ namespace Hello_Operators_advstud
             // random.Next(MaxValue) returns a 32-bit signed integer that is greater than or equal to 0 and less than MaxValue
             int Guess_number = random.Next(MyMax) + 1;
             // implement input of number and comparison result message in the while circle with  comparison condition
-
-            var guessed = false;
             
-            var avalableColors = new ConsoleColor[] { ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Magenta };
+            var avaliableColors = new ConsoleColor[] { ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Red, ConsoleColor.Magenta };
             var colorCounter = 0;
 
-            do
+            while (true)
             {
-                if (colorCounter == (avalableColors.Length))
+                colorCounter++;
+
+                if (colorCounter == (avaliableColors.Length))
                     colorCounter = 0;
 
-                Console.ForegroundColor = avalableColors[colorCounter];
+                Console.ForegroundColor = avaliableColors[colorCounter];
 
                 Console.WriteLine("Enter your guess: ");
 
@@ -37,18 +37,17 @@ namespace Hello_Operators_advstud
                 if (tryGuess == Guess_number)
                 {
                     Console.WriteLine("Congratulations!!!");
-                    guessed = true;
+                    return;
                 }
-                else if (tryGuess < Guess_number)
+
+                if (tryGuess < Guess_number)
                 {
                     Console.WriteLine("Too low!");
+                    continue;
                 }
-                else
-                    Console.WriteLine("Too high");
 
-                colorCounter++;
-
-            } while (!guessed);
+                Console.WriteLine("Too high");
+            } 
         }
     }
 }
