a4d1491fec3b1c2e5fd0585657092f0cdd340365
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2022-01-09 18:03:41 +0200

    Strange task

---
+++ CSharp_Net_module1_3_1_lab/CSharp_Net-module1_3_1_lab/CallingExeptionTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_Net_module1_3_1_lab
+{
+    enum CallingExeptionTypes 
+    {
+        OutOfRange = 1,
+        DivisionByZero,
+        NullReference,
+        NoException
+    }
+}
