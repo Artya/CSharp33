67f0dcb043c389886c92f3931c348e32e842729f
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2022-01-12 20:01:35 +0200

    Just experiment

---
+++ TestEventsAndDelegates/TestEventsAndDelegates/Home.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace TestEventsAndDelegates
+{
+    public delegate void SomeBodyCame(Inhabitant inhabitant);
+
+    public class Home
+    {
+        private Inhabitant[] homeInhabitants;
+        private int currentInhabitantIndex;
+
+        public event SomeBodyCame HandleSomeBodyCome; // (Inhabitant inhabitant);
+        
+
+        public Home(int inhabitansCount)
+        {
+            currentInhabitantIndex = default;
+            homeInhabitants = new Inhabitant[inhabitansCount];
+        }
+
+        public void AddInhabitant(Inhabitant inhabitant)
+        {
+
+            if (currentInhabitantIndex == homeInhabitants.Length)
+                return;
+
+            homeInhabitants[currentInhabitantIndex] = inhabitant;
+            currentInhabitantIndex++;
+            HandleSomeBodyCome += inhabitant.ReactionToSomeBodyCome;
+
+            if (HandleSomeBodyCome != null)
+            {
+                Console.WriteLine("Before HandleSomeBodyCome");
+                HandleSomeBodyCome(inhabitant);
+                Console.WriteLine("After HandleSomeBodyCome");
+                Console.WriteLine();
+            }
+        }
+
+        public void ReMoveInhabitant(Inhabitant inhabitant)
+        {
+            var index = Array.IndexOf(homeInhabitants, inhabitant);
+
+            if (index < 0)
+            {
+                return;
+            }
+
+            currentInhabitantIndex--;
+            homeInhabitants[index] = null;
+            HandleSomeBodyCome -= inhabitant.ReactionToSomeBodyCome;
+
+            if (index == homeInhabitants.Length - 1)
+            {
+                return;
+            }
+
+            var elementsLeft = homeInhabitants.Length - index - 1;
+
+            Array.Copy(homeInhabitants, index + 1, homeInhabitants, index, elementsLeft);
+
+            homeInhabitants[index + elementsLeft] = null;
+        }
+    }
+}
---
+++ TestEventsAndDelegates/TestEventsAndDelegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestEventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var home = new Home(4);
+
+            var dog = new Inhabitant(InhabitantType.Dog, "Bobik");
+            home.AddInhabitant(dog);
+
+            var cat = new Inhabitant(InhabitantType.Cat, "Murzik");
+            home.AddInhabitant(cat);
+
+            var father = new Inhabitant(InhabitantType.Human, "Father");
+            home.AddInhabitant(father);
+
+            var mother = new Inhabitant(InhabitantType.Human, "Mother");
+            home.AddInhabitant(mother);
+
+            home.ReMoveInhabitant(mother);
+            home.ReMoveInhabitant(father);
+
+            home.AddInhabitant(mother);
+            home.AddInhabitant(father);
+        }
+    }
+}
