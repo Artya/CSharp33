91b1cccc589acf3aa2210fa40bcd49f799873b48
kievzenit <contact@kievzenit.tk>
2022-01-29 18:38:40 +0200

    files added

---
+++ AnimalSorting/AnimalSorting/Animals.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSorting
+{
+    public class Animals : IEnumerable<Animal>
+    {
+        private Animal[] animals;
+
+        public Animals(Animal[] animals)
+        {
+            if (animals == null)
+                throw new ArgumentNullException("animals array can't be null");
+
+            this.animals = animals;
+        }
+        public IEnumerator<Animal> GetEnumerator()
+        {
+            return new Enumerator(this.animals);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        class Enumerator : IEnumerator<Animal>
+        {
+            private int position;
+            private Animal[] animals;
+
+            public Animal Current { get => animals[position]; }
+
+            object IEnumerator.Current => this.Current;
+
+            public Enumerator(Animal[] animals)
+            {
+                this.position = -1;
+                this.animals = animals;
+            }
+
+            public void Dispose()
+            {
+                this.Reset();
+            }
+
+            public bool MoveNext()
+            {
+                position++;
+                if (position >= this.animals.Length)
+                    return false;
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                this.position = 0;
+            }
+        }
+    }
+}
---
+++ AnimalSorting/AnimalSorting/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AnimalSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animalsArray = new Animal[]
+            {
+                new Animal("g1", 87),
+                new Animal("g2", 151),
+                new Animal("g0", 100),
+            };
+            var animals = new Animals(animalsArray);
+
+            Console.WriteLine("Unsorted array:");
+            printAnimals(animals);
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted by genus, ascending:");
+            Array.Sort(animalsArray);
+            printAnimals(animals);
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted by genus, descending:");
+            Array.Sort(animalsArray, Animal.SortByGenusDescending());
+            printAnimals(animals);
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted by weight, ascending:");
+            Array.Sort(animalsArray, Animal.SortByWeightAscending());
+            printAnimals(animals);
+            Console.WriteLine();
+        }
+
+        private static void printAnimals(Animals animals)
+        {
+            foreach (var animal in animals)
+                Console.WriteLine($"weight: {animal.Weight}, genus: {animal.Genus}");
+        }
+    }
+}
