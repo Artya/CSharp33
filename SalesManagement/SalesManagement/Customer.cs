0264ecc94efd23be0de7bf4fd75de895b3dfd1ca
kievzenit <contact@kievzenit.tk>
2022-01-29 19:06:46 +0200

    files added

---
+++ SalesManagement/SalesManagement/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SalesManagement
+{
+    public class Customer
+    {
+        public readonly string Name;
+
+        public Customer(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("Name can't be null.");
+
+            this.Name = name;
+        }
+
+        public void GotNewGoods(object sender, GoodsInfoEventArgs eventArgs)
+        {
+            Console.WriteLine($"{this.Name} got new goods: {eventArgs.GoodsName}");
+        }
+    }
+}
---
+++ SalesManagement/SalesManagement/GoodsInfoEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SalesManagement
+{
+    public class GoodsInfoEventArgs : EventArgs
+    {
+        public string GoodsName { get; private set; }
+
+        public GoodsInfoEventArgs(string goodsName)
+        {
+            if (goodsName == null)
+                throw new ArgumentNullException("goodsName can't be null.");
+
+            this.GoodsName = goodsName;
+        }
+    }
+}
