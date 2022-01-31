970872895fd9f27e6da5bc0bbece7f87113d3127
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2022-02-01 01:47:56 +0200

    Animal comparability

---
+++ CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Animals.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp_Net_module1_4_3_lab
+{
+    class Animals : IEnumerable
+    {
+        private Animal[] animals;
+        public int Length { get => animals.Length; }
+
+        public Animal this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > animals.Length)
+                    throw new IndexOutOfRangeException($"Index {index} is out of range");
+
+                return animals[index];
+            }
+        }
+
+        public Animals(Animal[] inputAnimals)
+        {
+            this.animals = inputAnimals;
+        }
+
+        public void Sort(SortTypes sortType)
+        {
+            switch (sortType)
+            {
+                case SortTypes.WeightAscending:
+                    Array.Sort(this.animals, Animal.SortWeightAscending);
+                    break;
+
+                case SortTypes.GenusDescending:
+                    Array.Sort(this.animals, Animal.SortGenusDescending);
+                    break;
+
+                default:
+                    Array.Sort(this.animals);
+                    break;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var currentAnimal in this.animals)
+            {
+                yield return currentAnimal;
+            }
+        }
+    }
+}
