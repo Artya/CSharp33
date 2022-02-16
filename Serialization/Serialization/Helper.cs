42a45ef7cf4708b21f0229299a243a62f9332d90
kievzenit <contact@kievzenit.tk>
2022-02-16 19:27:48 +0200

    files added

---
+++ Serialization/Serialization/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace Serialization
+{
+    public static class Helper
+    {
+        private static XmlSerializer xmlFormatter;
+        private static BinaryFormatter binaryFormatter;
+
+        static Helper()
+        {
+            xmlFormatter = new XmlSerializer(typeof(Student));
+            binaryFormatter = new BinaryFormatter();
+            
+        }
+
+        public static void SerializeToXml(Student student, Stream stream)
+        {
+            using (stream)
+            {
+                xmlFormatter.Serialize(stream, student);
+            }
+        }
+        public static Student DeserializeXml(Stream stream)
+        {
+            var student = xmlFormatter.Deserialize(stream) as Student;
+
+            return student;
+        }
+        public static void SerializeToBinary(Student student, Stream stream)
+        {
+            using (stream)
+            {
+                try
+                {
+                    binaryFormatter.Serialize(stream, student);
+                }
+                catch (SerializationException ex)
+                {
+                    Console.WriteLine($"Error while serializing to binary format, error: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+        public static Student DeserializeBinary(Stream stream)
+        {
+            var student = binaryFormatter.Deserialize(stream) as Student;
+            return student;
+        }
+    }
+}
---
+++ Serialization/Serialization/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Student
+    {
+        [XmlIgnore]
+        public string FirstName;
+        public string LastName;
+        [NonSerialized]
+        public string Nationality;
+
+        private string Address;
+        private string Code;
+
+        public Student() { }
+        public Student(string firstName, string lastName, string nationality, string address, string code)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Nationality = nationality;
+            this.Address = address;
+            this.Code = code;
+        }
+
+        public override string ToString()
+        {
+            return $"Fisrt name: {this.FirstName}, last name: {this.LastName}, nationality: {this.Nationality}, address: {this.Address}, code: {this.Code}";
+        }
+    }
+}
