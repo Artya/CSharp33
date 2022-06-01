d4fac1326f3a86eaebd0bf457af539a9082074a3
kievzenit <contact@kievzenit.tk>
2022-06-01 20:21:38 +0300

    files added

---
+++ UnsafeCode/UnsafeCode/PointerOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UnsafeCode
+{
+    public static class PointerOperation
+    {
+        public static int? NullableInt;
+        public static int NotNullableInt;
+
+        public static byte[] ConvertToByte(int number)
+        {
+            var bytes = BitConverter.GetBytes(number);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+
+            return bytes;
+        }
+
+        public unsafe static int Power(int* pointerToNumber, int power)
+        {
+            if (power == 0)
+                return 1;
+
+            var result = *pointerToNumber;
+            for (var i = 1; i < power; i++)
+                result *= *pointerToNumber;
+
+            return result;
+        }
+
+        public static void SetAsNull()
+        {
+            NotNullableInt = NullableInt ?? 0;
+        }
+    }
+}
---
+++ UnsafeCode/UnsafeCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            var number = 5;
+            var pointerToNumber = &number;
+
+            Console.WriteLine(PointerOperation.Power(pointerToNumber, 3));
+
+            PointerOperation.NullableInt = null;
+            PointerOperation.SetAsNull();
+
+            var bytes = PointerOperation.ConvertToByte(301);
+
+            foreach (var b in bytes)
+                Console.Write($"{b} ");
+
+            Console.WriteLine();
+        }
+    }
+}
