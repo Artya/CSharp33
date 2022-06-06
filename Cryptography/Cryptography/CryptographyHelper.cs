2449e18e277bf0e633875008c5068c974043f7cb
kievzenit <contact@kievzenit.tk>
2022-06-06 17:57:53 +0300

    files added

---
+++ Cryptography/Cryptography/CryptographyHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Cryptography
+{
+    public static class CryptographyHelper
+    {
+        public const byte MD5HashLength = 16;
+        public const byte HMACSHA256HashLength = 32;
+
+        public static byte[] MD5Hash(byte[] data)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return md5.ComputeHash(data);
+            }
+        }
+
+        public static bool MD5HashCheck(byte[] data, byte[] hash)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var bytes = md5.ComputeHash(data);
+
+                for (var i = 0; i < MD5HashLength; i++)
+                    if (bytes[i] != hash[i])
+                        return false;
+
+                return true;
+            }
+        }
+
+        public static byte[] Sign(byte[] data, string password)
+        {
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(password)))
+            {
+                return hmac.ComputeHash(data);
+            }
+        }
+
+        public static bool SignCheck(byte[] data, string password, byte[] hash)
+        {
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(password)))
+            {
+                var bytes = hmac.ComputeHash(data);
+
+                for (var i = 0; i < HMACSHA256HashLength; i++)
+                    if (bytes[i] != hash[i])
+                        return false;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Cryptography/Cryptography/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string password = "somestrongpassword";
+
+            var serializer = new XmlSerializer(typeof(Person));
+
+            var petya = new Person()
+            {
+                Name = "Petya",
+                Lastname = "Pupkin"
+            };
+            var vasya = default(Person);
+
+            var normalData = default(byte[]);
+            var signedData = default(byte[]);
+
+            var md5UnsignedHash = new byte[CryptographyHelper.MD5HashLength];
+            var md5SignedHash = new byte[CryptographyHelper.MD5HashLength];
+            var hmacHash = new byte[CryptographyHelper.HMACSHA256HashLength];
+
+            using (var fileStream = File.Open("person.xml", FileMode.Create))
+            {
+                serializer.Serialize(fileStream, petya);
+            }
+
+            using (var fileStream = File.Open("person.xml", FileMode.Open))
+            {
+                var buffer = new byte[fileStream.Length];
+                fileStream.Read(buffer, 0, buffer.Length);
+                normalData = buffer;
+            }
+
+            md5UnsignedHash = CryptographyHelper.MD5Hash(normalData);
+            Console.WriteLine($"person.xml hash: {BitConverter.ToString(md5UnsignedHash)}");
+
+            using (var fileStream = File.Open("person.signed.xml", FileMode.Create))
+            {
+                hmacHash = CryptographyHelper.Sign(normalData, password);
+
+                fileStream.Write(hmacHash);
+                fileStream.Write(normalData);
+            }
+
+            using (var fileStream = File.Open("person.signed.xml", FileMode.Open))
+            {
+                var buffer = new byte[fileStream.Length];
+                fileStream.Read(buffer, 0, buffer.Length);
+                signedData = buffer;
+            }
+
+            md5SignedHash = CryptographyHelper.MD5Hash(signedData);
+
+            Console.WriteLine($"person.signed.xml hash: {BitConverter.ToString(md5SignedHash)}");
+            Console.WriteLine($"Is person.xml equals to person.signed.xml? Answer: {CryptographyHelper.MD5HashCheck(signedData, md5UnsignedHash)}");
+
+            using (var fileStream = File.Open("person.signed.xml", FileMode.Open))
+            {
+                fileStream.Position = CryptographyHelper.HMACSHA256HashLength;
+
+                vasya = (Person)serializer.Deserialize(fileStream);
+                vasya.Name = "Vasya";
+            }
+
+            using (var fileStream = File.Open("person.signed.modified.xml", FileMode.Create))
+            {
+                fileStream.Write(hmacHash);
+                serializer.Serialize(fileStream, vasya);
+            }
+
+            using (var fileStream = File.Open("person.signed.modified.xml", FileMode.Open))
+            {
+                var buffer = new byte[fileStream.Length - CryptographyHelper.HMACSHA256HashLength];
+                fileStream.Position = CryptographyHelper.HMACSHA256HashLength;
+                fileStream.Read(buffer, 0, buffer.Length);
+
+                Console.WriteLine($"Is person.signed.modified.xml signed right? Answer: {CryptographyHelper.SignCheck(buffer, password, hmacHash)} ");
+            }
+        }
+    }
+}
