96db54439a9a4ad2a124fd44c65a365dd01b36c6
Сергій Симчук <Sergiy.Simchuk@gmail.com>
2022-06-10 23:40:07 +0300

    Code first

---
+++ Lab_5_5_EF_CodeFirst/Lab_5_5_EF_CodeFirst/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_5_EF_CodeFirst
+{
+    [Table("Customers")]
+    internal class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Details { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
